42b7c0365173e2363e29f7bc0553f947ad58bf09
Johan Fogelström <zapmew@gmail.com>
2015-07-21 17:31:57 +0200

    Extracted routes from main file and cleaned up the routes using gorilla/mux

--- main.go
+++ main.go
@@ -3,24 +3,18 @@ package main
 import (
 	"flag"
 	"github.com/Sirupsen/logrus"
+	"github.com/gorilla/securecookie"
 	"github.com/haisum/recaptcha"
 	"github.com/rifflock/lfshook"
-	"log"
 	"net/http"
-	"net/http/httputil"
 	"os"
 	"strings"
-
-	"github.com/gorilla/context"
-	"github.com/gorilla/securecookie"
-	"github.com/gorilla/sessions"
 )
 
 var (
 	cfile      = flag.String("f", defaultcfile, "Config file to use")
 	setup      = flag.Bool("setup", false, "creates a config-file with key")
 	logger     *logrus.Logger
-	store      sessions.Store
 	users      UserManager
 	recaptcher recaptcha.R
 )
@@ -30,12 +24,13 @@ func init() {
 	logger.Out = os.Stderr
 	logger.Formatter = &logrus.TextFormatter{}
 	logger.Level = logrus.InfoLevel
-}
 
-func main() {
 	flag.Parse()
 	loadConfig()
 	setupLogger()
+}
+
+func main() {
 	if *setup {
 		config.Keys.AuthenticationKey = securecookie.GenerateRandomKey(64)
 		config.Keys.EncryptionKey = securecookie.GenerateRandomKey(32)
@@ -81,162 +76,4 @@ func setupLogger() {
 	logger.Hooks.Add(lfshook.NewHook(lfshook.PathMap{
 		logrus.ErrorLevel: config.Logfile,
 	}))
-}
-
-const (
-	loginPath = "/proxy/login"
-)
-
-func setupHandlers() http.Handler {
-	store = sessions.NewCookieStore(config.Keys.AuthenticationKey, config.Keys.EncryptionKey)
-	muxer := http.NewServeMux()
-	w := logger.Writer()
-	//FIXME Should somehow close w.
-	revProxy := &httputil.ReverseProxy{
-		Director: func(r *http.Request) {
-			r.URL.Scheme = "http"
-			r.URL.Host = config.Destination
-		},
-		ErrorLog: log.New(w, "", 0),
-	}
-
-	muxer.Handle("/", context.ClearHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger.WithFields(logrus.Fields{
-			"method": r.Method,
-			"url":    r.URL,
-			"client": r.RemoteAddr,
-		}).Info()
-		session, _ := store.Get(r, "auth")
-
-		if loggedin, ok := session.Values["loggedin"].(bool); !(ok && loggedin) {
-			if isWebsocket(r) {
-				http.Error(w, "You need to login first.", http.StatusUnauthorized)
-			}
-			logger.WithFields(logrus.Fields{
-				"method":   r.Method,
-				"url":      r.URL,
-				"client":   r.RemoteAddr,
-				"redirect": loginPath,
-				"status":   http.StatusTemporaryRedirect,
-			}).Info("Client not logged in.")
-			http.Redirect(w, r, loginPath, http.StatusTemporaryRedirect)
-		}
-
-		if isWebsocket(r) {
-			p := websocketProxy{}
-			p.ServeHTTP(w, r)
-			return
-		}
-		revProxy.ServeHTTP(w, r)
-	})))
-
-	muxer.Handle(loginPath, context.ClearHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger.WithFields(logrus.Fields{
-			"method": r.Method,
-			"url":    r.URL,
-			"client": r.RemoteAddr,
-		}).Info("Request to login handler")
-		switch r.Method {
-		case "GET":
-			getLogin(w, r)
-		case "POST":
-			postLogin(w, r)
-		}
-	})))
-
-	muxer.Handle("/proxy/register", context.ClearHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger.WithFields(logrus.Fields{
-			"method": r.Method,
-			"url":    r.URL,
-			"client": r.RemoteAddr,
-		}).Info("Request to register handler")
-
-		switch r.Method {
-		case "GET":
-			getRegister(w, r)
-		case "POST":
-			postRegister(w, r)
-		}
-	})))
-
-	return muxer
-}
-
-func getLogin(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "auth")
-	if loggedin, ok := session.Values["loggedin"].(bool); ok && loggedin {
-		//Already logged in, so redirect to mainpage.
-		logger.WithFields(logrus.Fields{
-			"method":   r.Method,
-			"url":      r.URL,
-			"client":   r.RemoteAddr,
-			"redirect": "/",
-			"status":   http.StatusTemporaryRedirect,
-		}).Info("Client already logged in.")
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-	w.Write([]byte(loginPage)) //Not logged in. Serve login page
-}
-
-func postLogin(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "auth")
-	//Temp code. Autologin.
-	r.ParseForm()
-
-	if users.Authenticate(r.PostFormValue("username"), r.PostFormValue("password")) {
-		session.Values["loggedin"] = true
-		session.Save(r, w)
-		logger.WithFields(logrus.Fields{
-			"method": r.Method,
-			"url":    r.URL,
-			"client": r.RemoteAddr,
-			"user":   r.PostFormValue("username"),
-		}).Info("Client logged in.")
-		w.Write([]byte(loginSuccessPage))
-	} else {
-		logger.WithFields(logrus.Fields{
-			"method": r.Method,
-			"url":    r.URL,
-			"client": r.RemoteAddr,
-			"user":   r.PostFormValue("username"),
-		}).Info("Client failed to logged in.")
-		w.Write([]byte(loginPage))
-	}
-}
-
-func getRegister(w http.ResponseWriter, r *http.Request) {
-	//If they are logged in and want to register again, then fine.
-	//Can add measures against this if it becomes and issue.
-	w.Write([]byte(registrationPage)) //Serve register page
-}
-
-func postRegister(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	username, password := r.PostFormValue("username"), r.PostFormValue("password")
-	if !recaptcher.Verify(*r) {
-		logger.WithFields(logrus.Fields{
-			"user":  username,
-			"error": recaptcher.LastError(),
-		}).Error("Failed to verify reCaptcha during registration.")
-		w.Write([]byte("Failed to verify the reCaptcha. Please verify that you are human and try again."))
-		return
-	}
-
-	err := users.Register(username, password)
-	switch err {
-	case nil:
-		//Success
-		logger.WithFields(logrus.Fields{
-			"method": r.Method,
-			"url":    r.URL,
-			"client": r.RemoteAddr,
-			"user":   username,
-		}).Info("User registration")
-		w.Write([]byte(registrationSuccessPage))
-	case ErrUserExists:
-		http.Error(w, "The user already exists. Please try again with a different username.", http.StatusPreconditionFailed)
-	default:
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
 }
---
+++ routes.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+const (
+	loginPath = "/login"
+)
+
+var (
+	store    sessions.Store
+	revProxy *httputil.ReverseProxy
+)
+
+func setupHandlers() http.Handler {
+	store = sessions.NewCookieStore(config.Keys.AuthenticationKey, config.Keys.EncryptionKey)
+	muxer := mux.NewRouter()
+	w := logger.Writer()
+	//FIXME Should somehow close w. Go routine listening to channel closed at end of program?
+	revProxy = &httputil.ReverseProxy{
+		Director: func(r *http.Request) {
+			r.URL.Scheme = "http"
+			r.URL.Host = config.Destination
+		},
+		ErrorLog: log.New(w, "", 0),
+	}
+
+	muxer.Handle("/", LoggingMiddleware{http.HandlerFunc(mainHandler)})
+	muxer.Handle("/{x}", LoggingMiddleware{http.HandlerFunc(mainHandler)}) //Both are necessary.
+
+	proxymux := muxer.PathPrefix("/proxy").Subrouter()
+	proxymux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("AuthProx - Menu"))
+	})
+
+	{
+		m := proxymux.Methods("GET").Subrouter()
+		m.PathPrefix("/login").Handler(LoggingMiddleware{http.HandlerFunc(getLogin)})
+		m.PathPrefix("/register").Handler(LoggingMiddleware{http.HandlerFunc(getRegister)})
+	}
+	{
+		m := proxymux.Methods("POST").Subrouter()
+		m.PathPrefix("/login").Handler(LoggingMiddleware{http.HandlerFunc(postLogin)})
+		m.PathPrefix("/register").Handler(LoggingMiddleware{http.HandlerFunc(postRegister)})
+	}
+
+	return muxer
+}
+
+type LoggingMiddleware struct {
+	Wrapped http.Handler
+}
+
+func (lm LoggingMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	logger.WithFields(logrus.Fields{
+		"method": r.Method,
+		"url":    r.URL,
+		"client": r.RemoteAddr,
+	}).Info("HTTP Request")
+	lm.Wrapped.ServeHTTP(w, r)
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "auth")
+
+	if loggedin, ok := session.Values["loggedin"].(bool); !(ok && loggedin) {
+		if isWebsocket(r) {
+			http.Error(w, "You need to login first.", http.StatusUnauthorized)
+		}
+		logger.WithFields(logrus.Fields{
+			"method":   r.Method,
+			"url":      r.URL,
+			"client":   r.RemoteAddr,
+			"redirect": "/proxy/login",
+			"status":   http.StatusTemporaryRedirect,
+		}).Info("Client not logged in.")
+		http.Redirect(w, r, "/proxy/login", http.StatusTemporaryRedirect)
+	}
+
+	if isWebsocket(r) {
+		p := websocketProxy{}
+		p.ServeHTTP(w, r)
+		return
+	}
+	revProxy.ServeHTTP(w, r)
+}
+
+func getLogin(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "auth")
+	if loggedin, ok := session.Values["loggedin"].(bool); ok && loggedin {
+		//Already logged in, so redirect to mainpage.
+		logger.WithFields(logrus.Fields{
+			"method":   r.Method,
+			"url":      r.URL,
+			"client":   r.RemoteAddr,
+			"redirect": "/",
+			"status":   http.StatusTemporaryRedirect,
+		}).Info("Client already logged in.")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	w.Write([]byte(loginPage)) //Not logged in. Serve login page
+}
+
+func postLogin(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "auth")
+	//Temp code. Autologin.
+	r.ParseForm()
+
+	if users.Authenticate(r.PostFormValue("username"), r.PostFormValue("password")) {
+		session.Values["loggedin"] = true
+		session.Save(r, w)
+		logger.WithFields(logrus.Fields{
+			"method": r.Method,
+			"url":    r.URL,
+			"client": r.RemoteAddr,
+			"user":   r.PostFormValue("username"),
+		}).Info("Client logged in.")
+		w.Write([]byte(loginSuccessPage))
+	} else {
+		logger.WithFields(logrus.Fields{
+			"method": r.Method,
+			"url":    r.URL,
+			"client": r.RemoteAddr,
+			"user":   r.PostFormValue("username"),
+		}).Info("Client failed to logged in.")
+		w.Write([]byte(loginPage))
+	}
+}
+
+func getRegister(w http.ResponseWriter, r *http.Request) {
+	//If they are logged in and want to register again, then fine.
+	//Can add measures against this if it becomes and issue.
+	w.Write([]byte(registrationPage)) //Serve register page
+}
+
+func postRegister(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username, password := r.PostFormValue("username"), r.PostFormValue("password")
+	if !recaptcher.Verify(*r) {
+		logger.WithFields(logrus.Fields{
+			"user":  username,
+			"error": recaptcher.LastError(),
+		}).Error("Failed to verify reCaptcha during registration.")
+		w.Write([]byte("Failed to verify the reCaptcha. Please verify that you are human and try again."))
+		return
+	}
+
+	err := users.Register(username, password)
+	switch err {
+	case nil:
+		//Success
+		logger.WithFields(logrus.Fields{
+			"method": r.Method,
+			"url":    r.URL,
+			"client": r.RemoteAddr,
+			"user":   username,
+		}).Info("User registration")
+		w.Write([]byte(registrationSuccessPage))
+	case ErrUserExists:
+		http.Error(w, "The user already exists. Please try again with a different username.", http.StatusPreconditionFailed)
+	default:
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
